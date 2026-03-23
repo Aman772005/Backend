58d9afae236242d395dbd004d073e602904eb570
Aman772005 <amansinghtomar772005@gmail.com>
2026-03-23 18:32:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let n = 5 ;
+
+// for (i=1;i<=n;i++){
+//     console.log("hello backend",i)
+// }
+
+// let args = process.argv ;
+
+// for(let i = 2;i<args.length ; i++){
+//     console.log("hello",args[i]);
+// }
+
+// const someValue = require("./math");
+
+// console.log(someValue.PI);
+// console.log(someValue.G);
+// console.log(someValue.sum(2,4));
+// console.log(someValue.mul(3,4));
+
+const info = require("./Fruits")
+
+console.log(info[0].name)
+
+// completed
+// nested is completed
+
+
+
+
+
