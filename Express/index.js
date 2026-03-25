07fe541ee7c3194f7a867813348c19561a581c96
Aman772005 <amansinghtomar772005@gmail.com>
2026-03-25 22:06:21 +0530

    Express completed , EJS start

--- Express/index.js
+++ Express/index.js
@@ -9,13 +9,58 @@ app.listen(port ,()=>{
     console.log(`app is lisenting on port ${port}`);
 });
 
-app.use((req,res)=>{
-    //console.log(req)
-    console.log("request received");
-    let obj = {
-        name : "Aman",
-        age : 20 ,
-    }
-    let code = "<h1>Friuts</h1> <ul><li>orange</li><li>apple</li></ul>"
-    res.send( code);
+// app.use((req,res)=>{
+//     //console.log(req)
+//     console.log("request received");
+//     let obj = {
+//         name : "Aman",
+//         age : 20 ,
+//     }
+//     let code = "<h1>Friuts</h1> <ul><li>orange</li><li>apple</li></ul>"
+//     res.send( code);
+// });
+
+app.get("/",(req,res)=>{
+    res.send("you contected root path")
+})
+
+app.get("/apple",(req,res)=>{
+    res.send("you contected apple path")
+})
+
+app.get("/banana",(req,res)=>{
+    res.send("you contected banana path")
+})
+
+// ***  this is for old version now it is not working *** // 
+
+// app.get("/*",(req,res)=>{
+//     res.send("you are on wrong path");
+// });
+
+// app.get('/*', (req, res) => {
+//   res.send('Not Found');
+// });
+
+// app.use((req, res) => {
+//   res.status(404).send('this is not avilable');
+// });
+
+app.post("/", (req, res) => {
+    res.send("you post ");
 });
+
+app.get(("/:username/:id"),(req,res)=>{
+    let {username , id} = req.params ;
+    let htmlstr = `<h1>welcome to the page of ${username}, of id = ${id}</h1>`
+    res.send(htmlstr);
+});
+
+app.get("/search",(req,res)=>{
+    let {q}= req.query ;
+    if(!q){
+        res.send(`<h1>Nothing have been searched</h1>`)
+    }
+    res.send(`<h1>this is query ${q} </h1>`);
+});
+
