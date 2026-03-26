7f90bce78c58966434fe1f0e8b4b6f88d17f34ad
Aman772005 <amansinghtomar772005@gmail.com>
2026-03-26 23:06:17 +0530

    EJS (renders)

--- EJS/index.js
+++ EJS/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 3333 ;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
-
-app.get("/" , (req,res)=>{
-    res.send("THis is message")
-})
