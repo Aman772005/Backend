a8b8729de9e5b3d1ad1dec8cfe477067be9f878b
Aman772005 <amansinghtomar772005@gmail.com>
2026-03-30 20:33:21 +0530

    Post req

--- MISCELLANEOUS/Backend/index.js
+++ MISCELLANEOUS/Backend/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 const port = 1111 ;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.get("/register",(req,res)=>{
-    let{user,pass}=req.query
-    res.send(`standard GET response ! Welcome ${user}`);
-})
+    let{user,password}=req.query
+    res.send(`standard GET response ! Welcome ${user},${password}`);
+});
 
 app.post("/register",(req,res)=>{
-    res.send("standard POST response");
-})
+    let{user,password}=(req.body)
+    res.send(`standard POST response ${user},${password}`);
+});
 
 app.listen(port , ()=>{
     console.log(`listening to port :${port}`)
-})
+});
